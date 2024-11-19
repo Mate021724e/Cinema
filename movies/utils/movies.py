eb3351f88b87fb3c8a356403b662118794bb4f2a
Denys Kelba <98632389+Mate021724e@users.noreply.github.com>
2024-11-19 23:51:56 +0200

    added CRUD

--- movies/utils/movies.py
+++ movies/utils/movies.py
@@ -4,17 +4,24 @@ def get_all_movies():
     """Функція для отримання та виведення всіх фільмів з бази даних."""
     movies = Movie.objects.all()
 
-    # Перевіряємо, чи є фільми
     if movies.exists():
         movies_info = []
         for movie in movies:
             movie_data = {
                 'Title': movie.title,
-                'Director': movie.directors,
-                'Genres': ', '.join([genre.name for genre in movie.genres.all()]),
+                'Description': movie.description,
                 'Poster': movie.poster,
+                'Year': movie.year,
+                'Country': movie.country,
+                'Director': movie.directors,
+                'Actors': movie.actors,
+                'Genres': ', '.join([genre.name for genre in movie.genres.all()]),
+                'Category': movie.category.name if movie.category else "N/A",
+
             }
             movies_info.append(movie_data)
         return movies_info
     else:
         return "No movies found in the database."
+    
+    
